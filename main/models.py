b36e8f7f3f9921fe670a0e78e0cbeb9f7ca3dea7
Niklas Ziermann <n-ziermann@protonmail.com>
2019-07-21 17:14:22 +0200

    Created Usermodel

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	mail = models.EmailField(blank=False, unique=True)
+	password = models.CharField(max_length=100)
+	data = models.TextField()	#encrypted array in string format
+	def __str__(self):
+		return self.mail
