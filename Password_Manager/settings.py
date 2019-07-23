87d76ce981b59912977edb3d4b74ee7d37303cc5
Niklas Ziermann <n-ziermann@protonmail.com>
2019-07-23 13:00:04 +0200

    Added Client-Side Hashing
    Added Client-Side Hashing with 2x 2500
    PBKDF2 Iterations

--- Password_Manager/settings.py
+++ Password_Manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8d0rr_*si2u!zb@e(y$fmf!srwx4olzhro)tnz=5)o7q!ii%lp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+
