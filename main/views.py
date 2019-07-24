f68fe5cc262f861e4f23061ca5095a47d50821bb
Niklas Ziermann <n-ziermann@protonmail.com>
2019-07-24 15:48:02 +0200

    Added vault encryption

--- main/views.py
+++ main/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from main.forms import UserCreateForm
 from main.models import User
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512, HMAC
+from base64 import b64encode
 import re
-
 # Create your views here.
 def homepage(request):
 	
 	if request.method == "POST":
+		print("*"*50)
 		email = request.POST["Email"]
 		password = request.POST["Password"]
-
-		print("*"*50)
+		new = request.POST["new"]
+		if new == "true":
+			data = request.POST["Vault"]
+			print(data)
 		print(email)
 		print(request.POST["new"])
 		print(password)
@@ -19,15 +24,30 @@ def homepage(request):
 		#check if email format is valid
 		if re.search("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
 			print("Email is valid")
+			
+			# hashing the password again
+			hashedPasswordBytes = PBKDF2("password", bytes(email, 'utf-8'), dkLen=64, count=100000, prf=prf)
+			print(hashedPasswordBytes)
+			# turn hashed password into string to store in db
+			hashedPassword = b64encode(hashedPasswordBytes).decode('utf-8')
+			print(hashedPassword)
+			# check if user already exists
+			if len(User.objects.raw('SELECT * FROM main_user WHERE mail="' + email + '";')) != 0:
+				if new:
+					print("User already exists.")
+				else:
+					print("Username right, testing login")
+			else:
+				if new:
+					print("creating new user")
+					
+				else:
+					print("user doesnt exist yet")
+		
 		else:
 			print("Email is invalid")
 
-		# hashing the password again
-		####
-
-		# check if user already exists
-		if len(User.objects.raw('SELECT * FROM main_user WHERE mail="' + email + '";')) != 0:
-			print("User already exists.")
+		
 		print("*"*50)
 
 
@@ -37,3 +57,6 @@ def homepage(request):
 	return render(	request=request,
 					template_name="index.html",
 					context={"user_create":UserCreateForm})
+
+def prf(p,s):
+	return HMAC.new(p,s,SHA512).digest()
